8b607709f1b94be5a0d3e640fad3edf4293ee941
Yonatan Hershko <yonher8@gmail.com>
2024-05-29 16:08:50 +0300

    Change ball size w randomit

--- js/index.js
+++ js/index.js
@@ -1,18 +1,19 @@
-'use strik'
+'use strict'
 
-var ballHight = 50
-var ballWidth = 50
+var ballHight
+var ballWidth
+var ranNum = getRandomInt(20, 61)
 
 function onBallClick() {
     var ball = document.querySelector('.ball')
     if (ballHight + 50 <= 400 && ballWidth + 50 <= 400) {
-        ballHight += 50
-        ballWidth += 50
+        ballHight += ranNum
+        ballWidth += ranNum
     } else {
         ballHight = 100
         ballWidth = 100
     }
-
+    // console.log(ball);
     ball.style.width = ballHight + 'px'
     ball.style.height = ballWidth + 'px'
 }
--- js/util.js
+++ js/util.js
@@ -1,3 +1,4 @@
+'use strict'
 
 function getRandomInt(min, max) {
     min = Math.ceil(min)
