e613355d1860e0ff7c15a6a960cd102024c19e22
Yonatan Hershko <yonher8@gmail.com>
2024-05-29 15:51:51 +0300

    Limit Ball Size

--- js/index.js
+++ js/index.js
@@ -3,9 +3,16 @@
 var ballHight = 50
 var ballWidth = 50
 
-function onBallClick(ball) {
-    ballHight = ballHight + 50
-    ballWidth = ballWidth + 50
+function onBallClick() {
+    var ball = document.querySelector('.ball')
+    if (ballHight + 50 <= 400 && ballWidth + 50 <= 400) {
+        ballHight += 50
+        ballWidth += 50
+    } else {
+        ballHight = 100
+        ballWidth = 100
+    }
+
     ball.style.width = ballHight + 'px'
     ball.style.height = ballWidth + 'px'
 }
