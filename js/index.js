1727d6faf8cfd18afc9dca81f6e368f502bf70c5
Yonatan Hershko <yonher8@gmail.com>
2024-05-29 17:38:44 +0300

    Ball4 option

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 var ballHeight = 100
 var ballWidth = 100
 var ranNum = getRandomInt(20, 61)
+var ball1 = document.querySelector(".ball")
+var ball2 = document.querySelector(".ball2")
 
 function onBallClick(ball,maxDiameter) {
     if (ballHeight < maxDiameter) {
@@ -15,27 +17,35 @@ function onBallClick(ball,maxDiameter) {
         ballHeight = 100
         ballWidth = 100
     }
-    console.log(ball,maxDiameter);
 }
 
 function onBall3Click(){
-    var ball1 = document.querySelector(".ball")
-    var ball2 = document.querySelector(".ball2")
-
-     var tempColor = ball1.style.backgroundColor
-     var tempWidth = ball1.style.width
-     var tempHeight = ball1.style.height
+     var ball1Color = ball1.style.backgroundColor
+     var ball1Width = ball1.style.width
+     var ball1Height = ball1.style.height
  
      ball1.style.backgroundColor = ball2.style.backgroundColor
      ball1.style.width = ball2.style.width
      ball1.style.height = ball2.style.height
  
-     ball2.style.backgroundColor = tempColor
-     ball2.style.width = tempWidth
-     ball2.style.height = tempHeight
+     ball2.style.backgroundColor = ball1Color
+     ball2.style.width = ball1Width
+     ball2.style.height = ball1Height
 
 }
 
-function onBall4Click() {
+function onBall4Click(change,Reduced) {
+    if (ballHeight > Reduced) {
+        ballHeight -= ranNum
+        ballWidth -= ranNum
+        ball2.style.width = ballHeight + 'px'
+        ball2.style.height = ballWidth + 'px'
+        ball1.style.width = ballHeight + 'px'
+        ball1.style.height = ballWidth + 'px'
+    }
+    else {
+        ballHeight = 100
+        ballWidth = 100
+    }
     
 }
