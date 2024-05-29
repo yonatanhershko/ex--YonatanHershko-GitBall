67fcf1e4cfd97bbe8166c76c97b8fccdda79c0d1
Yonatan Hershko <yonher8@gmail.com>
2024-05-29 23:11:04 +0300

    add undo op

--- js/index.js
+++ js/index.js
@@ -1,49 +1,96 @@
 'use strict'
 
-var ballHeight = 100
-var ballWidth = 100
-var ball2Height = 100
-var ball2Width = 100
+var gBall1size = 100
+var gBall2size = 100
+
 var ranNum = getRandomInt(20, 61)
 var ranNum2 = getRandomInt(20, 61)
+
 var ball1 = document.querySelector(".ball")
 var ball2 = document.querySelector(".ball2")
+
 var interval
+
 var gState = []
 
 
+
+saveState()
+function saveState() {
+    var state = {}
+    state.ball1 = {
+        width: (ball1.style.width || gBall1size + 'px'),
+        height: (ball1.style.height || gBall1size + 'px'),
+        text: ball1.innerHTML,
+        color: (ball1.style.backgroundColor || 'blanchedalmond')
+    }
+    state.ball2 = {
+        width: (ball2.style.width || gBall2size + 'px'),
+        height: (ball2.style.height || gBall2size + 'px'),
+        text: ball2.innerHTML,
+        color: (ball2.style.backgroundColor || 'rgb(205, 253, 255)')
+    }
+    state.backgroundColor = (document.body.style.backgroundColor || '#252525')
+    gState.push(state)
+}
+
+
+function undo() {
+    if (!gState.length) return'make a move.'
+
+    var prevState = gState.pop()
+    
+    ball1.style.width = prevState.ball1.width
+    ball1.style.height = prevState.ball1.height
+    ball1.style.backgroundColor = prevState.ball1.color
+    ball1.innerHTML = prevState.ball1.text
+    
+    ball2.style.width = prevState.ball2.width
+    ball2.style.height = prevState.ball2.height
+    ball2.style.backgroundColor = prevState.ball2.color
+    ball2.innerHTML = prevState.ball2.text
+    document.body.style.backgroundColor = prevState.backgroundColor;
+
+    console.log('Undo successful.');
+    return 'Undo successful.'
+}
+
+
+
+
+
 function onBallClick(ball, maxDiameter) {
-    if (ballHeight < maxDiameter) {
+    if (gBall1size < maxDiameter) {
         ball.style.backgroundColor = getRandomColor()
-        ballHeight += ranNum
-        ballWidth += ranNum
-        ball.style.width = ballHeight + 'px'
-        ball.style.height = ballWidth + 'px'
-        ball1.innerHTML = ballHeight
+        gBall1size += ranNum
+        ball.style.width = gBall1size + 'px'
+        ball.style.height = gBall1size + 'px'
+        ball1.innerHTML = gBall1size
+        
     } else {
-        ballHeight = 100
-        ballWidth = 100
-        ball.style.width = ballHeight + 'px'
-        ball.style.height = ballWidth + 'px'
-        ball1.innerHTML = ballHeight
-    } return { ballHeight, ballWidth }
+        gBall1size = 100
+        ball.style.width = gBall1size + 'px'
+        ball.style.height = gBall1size + 'px'
+        ball1.innerHTML = gBall1size
+    } 
+    saveState()
 }
 
 function onBall2Click(ball, maxDiameter) {
-    if (ball2Height < maxDiameter) {
+    if (gBall2size < maxDiameter) {
         ball.style.backgroundColor = getRandomColor()
-        ball2Height += ranNum2
-        ball2Width += ranNum2
-        ball.style.width = ball2Height + 'px'
-        ball.style.height = ball2Width + 'px'
-        ball2.innerHTML = ball2Height
+        gBall2size += ranNum2
+        ball.style.width = gBall2size + 'px'
+        ball.style.height = gBall2size + 'px'
+        ball2.innerHTML = gBall2size
+        
     } else {
-        ball2Height = 100
-        ball2Width = 100
-        ball.style.width = ball2Height + 'px'
-        ball.style.height = ball2Width + 'px'
-        ball2.innerHTML = ball2Height
-    } return { ball2Height, ball2Width }
+        gBall2size = 100
+        ball.style.width = gBall2size + 'px'
+        ball.style.height = gBall2size + 'px'
+        ball2.innerHTML = gBall2size
+    } 
+    saveState()
 }
 
 
@@ -62,40 +109,41 @@ function onBall3Click() {
     ball2.style.width = ball1Width
     ball2.style.height = ball1Height
     ball2.innerHTML = ball1Text
-
+    saveState()
 }
 
 function onBall4Click(change, Reduced) {
-    if (ballHeight > Reduced) {
-        ballHeight -= ranNum
-        ballWidth -= ranNum
-        ball2Height -= ranNum2
-        ball2Width -= ranNum2
-        ball2.style.width = ball2Height + 'px'
-        ball2.style.height = ball2Width + 'px'
-        ball1.style.width = ballHeight + 'px'
-        ball1.style.height = ballWidth + 'px'
-        ball2.innerHTML = ball2Height
-        ball1.innerHTML = ballHeight
+    if (gBall1size > Reduced) {
+        gBall1size -= ranNum
+        gBall1size -= ranNum
+        gBall2size -= ranNum2
+        gBall2size -= ranNum2
+        ball2.style.width = gBall2size + 'px'
+        ball2.style.height = gBall2size + 'px'
+        ball1.style.width = gBall1size + 'px'
+        ball1.style.height = gBall1size + 'px'
+        ball2.innerHTML = gBall2size
+        ball1.innerHTML = gBall1size
     }
     else {
-        ballHeight = 100
-        ballWidth = 100
-        ball2Height = 100
-        ball2Width = 100
+        gBall1size = 100
+        gBall1size = 100
+        gBall2size = 100
+        gBall2size = 100
     }
 
 }
 
 function onBall5Click() {
     document.body.style.backgroundColor = getRandomColor()
+    saveState()
 }
 
 function onBall6Click() {
-    ballHeight = 100
-    ballWidth = 100
-    ball2Height = 100
-    ball2Width = 100
+    gBall1size = 100
+    gBall1size = 100
+    gBall2size = 100
+    gBall2size = 100
 
     ball1.style.width = "100px"
     ball1.style.height = "100px"
@@ -106,12 +154,12 @@ function onBall6Click() {
     ball2.style.backgroundColor = "rgb(205, 253, 255)"
 
     document.body.style.backgroundColor = "#252525"
+    saveState()
 }
 
 
 var intervalCount = 0
 function onBall6() {
-    console.log('hhh');
     interval = setTimeout(function () {
         interval = setInterval(function () {
             if (intervalCount > 10) {
@@ -127,44 +175,4 @@ function onBall6() {
         }, 2000)
     }, 2000)
 }
-
-function saveState() {
-    var state = {}
-    ball1 = {
-        width: (ball1.style.width || ballHeight + 'px'),
-        hight: (ball1.style.hight || ballHeight + 'px'),
-        text: ball1.innerHTML,
-        color: (ball1.style.backgroundColor || 'blanchedalmond')
-    }
-    ball2 = {
-        width: (ball2.style.width || ball2Height + 'px'),
-        hight: (ball2.style.hight || ball2Height + 'px'),
-        text: ball2.innerHTML,
-        color: (ball2.style.backgroundColor || 'rgb(205, 253, 255)')
-    }
-    state.backgroundColor = (document.body.style.backgroundColor || '#252525')
-    gState.push(state)
-}
-
-
-function undo() {
-    console.log('hh');
-    if (!gState.length) return 'Make a move'
-
-
-    var prevState = gState[gState.length - 1]
-
-    ball1.style.width = prevState.ball1.width
-    ball1.style.hight = prevState.ball1.hight
-    ball1.style.backgroundColor = prevState.ball1.color
-    ball1.innerHTML = prevState.ball1.text
-
-    ball2.style.width = prevState.ball2.width
-    ball2.style.hight = prevState.ball2.hight
-    ball2.style.backgroundColor = prevState.ball2.color
-    ball2.innerHTML = prevState.ball2.text
-
-
-}
-
 
