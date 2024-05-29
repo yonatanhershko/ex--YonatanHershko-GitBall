aeb732559eb9ace5a70b7fd3cc48d7fa66bb99b7
Yonatan Hershko <yonher8@gmail.com>
2024-05-29 15:10:40 +0300

    Ball Resize js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 'use strik'
 
+var ballHight = 50
+var ballWidth = 50
 
-function onBallClick(){
-    console.log('clicked');
+function onBallClick(ball) {
+    ballHight = ballHight + 50
+    ballWidth = ballWidth + 50
+    ball.style.width = ballHight + 'px'
+    ball.style.height = ballWidth + 'px'
 }
+
